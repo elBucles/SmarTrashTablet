1e47afa1323eb8d8108f017d1fcdcea65d402ca9
Bucles <DavidIsais@live.com>
2018-12-02 17:49:30 -0700

    Con sentencias de firebase añadidas

--- app/src/main/java/com/smarttrash/smartrash_tablet/NoRutas.java
+++ app/src/main/java/com/smarttrash/smartrash_tablet/NoRutas.java
@@ -4,13 +4,12 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Notification extends AppCompatActivity {
+public class NoRutas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
+        setContentView(R.layout.activity_no_rutas);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
     }
 }
